bd0115121a35d0db31c105bc455bebf6d212448d
mattrahendra <ismadirahendra@gmail.com>
2025-06-02 21:20:22 +0700

    v0.7.1

---
+++ resources/views/product/cart.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('components.head')
+    <title>Ring Bun - Keranjang</title>
+</head>
+
+<body>
+
+    <!-- Navbar Section -->
+    @include('components.nav')
+
+
+    <!-- Footer Section -->
+    @include('components.footer')
+</body>
+
+</html>
